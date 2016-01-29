c1a6670bb2c1c8ee1c51870bd6b9b7d17521746b
mobo <mobo@ubuntu.(none)>
2016-01-29 21:15:37 +0800

    configure for arm

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 Sun Feb 8 17:44:54 CST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (ctng-1.6.1) "
